0c6912040b135f25580627675930288663a7bc46
cicomel <155098755+cicomel@users.noreply.github.com>
2023-12-29 18:22:25 +0900

    Add files via upload

---
+++ data/TITIKKOORDINATKEJAKSAAN_4.js
@@ -0,0 +1,1 @@
+var json_TITIKKOORDINATKEJAKSAAN_4 = {"type":"FeatureCollection","name":"TITIKKOORDINATKEJAKSAAN_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"NAMA":"KEJAKSAAN NEGERI KEPULAUAN SELAYAR","X":120.46373,"Y":-6.11549},"geometry":{"type":"Point","coordinates":[120.46373,-6.115488]}}]}
